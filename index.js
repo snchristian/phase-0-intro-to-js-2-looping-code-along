f55cc4299f22a9a70225ea727405324a8b7f9e05
Shantel <snchristian8@gmail.com>
2021-07-28 21:39:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ['Lisa','Kaitlin','Jan'];
+const message = 'surprise';
+
+function writeCards(cards, messages){
+    let messagescopy = [];
+    for (let i = 0; i < cards.length; i++) {
+       messagescopy.push(`Thank you, ${cards[i]}, for the wonderful ${message} gift!`);
+    }
+    return messagescopy;
+}
+function countDown(int){
+    let i=10;
+    while(i >= 0){
+        console.log(i);
+        i--
+    }
+    return int;
+}
